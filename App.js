92a4b4dbd5bdec4e62a86ef327f59a1261a63698
Kev1n011 <90216024+Kev1n011@users.noreply.github.com>
2024-04-17 00:28:30 -0700

    Agrega nuevas funcionalidades de autenticación

--- App.js
+++ App.js
@@ -1,17 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 import React, { useState } from "react";
-import Finder from './Components/Finder.js';
+import Finder from './Components/Components/Finder.js';
 import PokemonCard from './Components/PokemonCard.js';
 
-function App() {
+export default function App() {
   const [pokemon, setPokemon] = useState(null);
 
   return (
     <div className="App">
-
       <Finder texto={"Buscar Pokémon"} foundPokemon={setPokemon} />
       {pokemon && <PokemonCard pokemon={pokemon} />}
     </div>
   );
 }
 
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
